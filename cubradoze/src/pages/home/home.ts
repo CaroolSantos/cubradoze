25fa13ce5b4399a1c636d7ed41c61e613e4d2ac2
Antonio <antonio.alves.correia@gmail.com>
2018-11-27 17:43:25 -0300

    sorteio de números

--- cubradoze/src/pages/home/home.ts
+++ cubradoze/src/pages/home/home.ts
@@ -6,9 +6,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  myCount = 6;
+  sorteando = false;
+  numero1: number;
+  numero2: number;
+  numero3: number;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  sortear() {
+    this.sorteando = true;
+
+    let interval = setInterval(() => {
+      this.myCount--;
+      this.numero1 = this.getRandomInt(1,6);
+      this.numero2 = this.getRandomInt(1,6);
+      this.numero3 = this.getRandomInt(1,6);
+
+      if (this.myCount == 0) clearInterval(interval);
+    }, 200)
+
+    
   }
 
 }
