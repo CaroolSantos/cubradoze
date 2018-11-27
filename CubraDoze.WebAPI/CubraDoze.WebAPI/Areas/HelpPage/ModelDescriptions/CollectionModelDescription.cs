fd0014a0d7e4787aacb7f5b9e03bb56b245d1fbb
Antonio Correia <antonio.alves.correia@gmail.com>
2018-11-27 14:42:22 -0300

    web

---
+++ CubraDoze.WebAPI/CubraDoze.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CubraDoze.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
